89805907207709a5c12bb17a7cf5e065c00da7f1
sanggu239 <romensch39@naver.com>
2022-06-15 16:11:40 +0900

    1. 로그인 화면 생성

---
+++ mainapp/mainapp/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from .views import HomeView
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
+]
